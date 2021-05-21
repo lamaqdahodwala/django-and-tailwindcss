d009e5c13b2d31e83cbc3e4f65456f7c3f659477
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-21 14:05:27 -0400

    updated js

--- project/main/static/main.js
+++ project/main/static/main.js
@@ -1,26 +1,31 @@
 let rolls = []
-
+let total_pts = 0
+let points_this_round = 0
+let round = 1
 function generate_random_number(){
     return Math.floor(Math.random()*6)+1
 }
 
-function turn(){
-    loop:
-    while (true){
-        let roll = generate_random_number()
-        if (roll == 6){
-            alert("oof you lost")
-            break loop;
-        } else {
-            rolls.push(roll)
-            if (!(confirm("roll again?"))){
-                break loop
-            }
-            let list = document.getElementById('pointboard')
-            let ele = document.createElement('li')
-            ele.innerHTML = roll
-            list.append(ele)
-        }
-    }
-    console.log(rolls)
+function handle_six(){
+    points_this_round = 0
+    next_round()
+
+}
+
+function roll(){
+    let roll = generate_random_number()
+    if (roll == 6) return handle_six()
+    rolls.push(roll)
+    points_this_round += roll
+}
+
+function end_game(){
+    //TODO: make end game screen
+}
+
+function next_round(){
+    if (round == 5) end_game()
+    round += 1
+    total_pts += points_this_round
+    points_this_round = 0
 }
