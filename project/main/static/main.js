6c08fba6fda068111a11cb894c28f8f4e1b1022d
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-21 14:18:18 -0400

    need to make ending screen now

--- project/main/static/main.js
+++ project/main/static/main.js
@@ -13,9 +13,11 @@ function handle_six(){
 
 function roll(){
     let roll = generate_random_number()
+    document.getElementById('last-roll').innerHTML = `Last roll: ${roll}`
     if (roll == 6) return handle_six()
     rolls.push(roll)
     points_this_round += roll
+    document.getElementById('current-total').innerHTML = `Current Total: ${total_pts+points_this_round}`
 }
 
 function end_game(){
@@ -26,6 +28,8 @@ function next_round(){
     if (round == 5) end_game()
     let id_for_pts = `round${round}points`
     document.getElementById(id_for_pts).innerHTML = `Round ${round}: ${points_this_round}`
+    document.getElementById(`round${round}indicator`).className = 'has-text-grey-light'
+    document.getElementById(`round${round+1}indicator`).className = 'has-text-primary-dark'
     round += 1
     total_pts += points_this_round
     points_this_round = 0
