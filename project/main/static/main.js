da5e2b38c313c51dd4009a1a133b25d279b1aab5
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-22 11:32:52 -0400

    new internal vars

--- project/main/static/main.js
+++ project/main/static/main.js
@@ -1,7 +1,16 @@
+import {Cookie} from 'js-cookie'
+
+
 let rolls = []
 let total_pts = 0
 let points_this_round = 0
 let round = 1
+let r1 = 0
+let r2 = 0
+let r3 = 0
+let r4 = 0
+let r5 = 0
+
 function generate_random_number(){
     return Math.floor(Math.random()*6)+1
 }
@@ -17,20 +26,45 @@ function roll(){
     if (roll == 6) return handle_six()
     rolls.push(roll)
     points_this_round += roll
-    document.getElementById('current-total').innerHTML = `Current Total: ${total_pts+points_this_round}`
+    document.getElementById('current-total').innerHTML = `Current Possible Total: ${total_pts+points_this_round}`
 }
 
 function end_game(){
-    //TODO: make end game screen
+    //Disable buttons
+    let roll_again_btn = document.getElementById('roll-again')
+    roll_again_btn.disabled = true
+    roll_again_btn.className = 'button is-disabled'
+
+    let end_turn_btn = document.getElementById('end-turn')
+    end_turn_btn.disabled = true
+    end_turn_btn.className = 'button is-disabled'
+    Cookie.set('score', total_pts)
 }
 
 function next_round(){
-    if (round == 5) end_game()
+    if (round == 5) return end_game()
     let id_for_pts = `round${round}points`
     document.getElementById(id_for_pts).innerHTML = `Round ${round}: ${points_this_round}`
     document.getElementById(`round${round}indicator`).className = 'has-text-grey-light'
     document.getElementById(`round${round+1}indicator`).className = 'has-text-primary-dark'
     round += 1
     total_pts += points_this_round
+    switch (round){
+        case 1:
+            r1 = points_this_round
+            break;
+        case 2:
+            r2 = points_this_round
+            break;
+        case 3:
+            r3 = points_this_round
+            break;
+        case 4: 
+            r4 = points_this_round
+            break;
+        case 5:
+            r5 = points_this_round
+            break;
+    }
     points_this_round = 0
 }
