4a16eff319909f0fa6a80381e40881ee0f6fe28c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-03 10:28:30 -0400

    reboot

--- project/main/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from .models import Score
-class ScoreModelForm(ModelForm):
-    class Meta:
-        model = Score
-        fields = ['total', 'r1', 'r2', 'r3', 'r4', 'r5']
