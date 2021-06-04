3ed911f1ba8c2b1b736272b494764b2a14054adb
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-03 20:22:33 -0400

    added rick roll shenangians

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
+
+def not_a_rickroll(req):
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
